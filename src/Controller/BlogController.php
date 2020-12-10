96b5cfabdd7856f1579532abeb14e50d7fa991a1
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2020-12-10 12:19:29 +0100

    Adding a form in the article creation page using Symfony

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,10 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\ArticleRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 
 class BlogController extends AbstractController
 {
@@ -34,8 +39,26 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/new", name="blog_create")
      */
-    public function create() {
-        return $this->render('blog/create.html.twig');
+    public function create(Request $request, EntityManagerInterface $manager) {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class ,[
+                'attr' => [
+                    'placeholder' => "Titre de l'article"
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => [
+                    'placeholder' => "Contentu de l'article"
+                ]
+            ])
+            ->add('image')
+            ->getForm();
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView()
+        ]);
     }
 
     /**
